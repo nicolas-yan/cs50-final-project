abcbe9c07b6efe42a7b457a32a237a15311aea30
nicolas-yan <43117197+nicolas-yan@users.noreply.github.com>
2018-11-28 19:32:22 -0500

    Add files via upload
    everything as before but with comments

--- content.js
+++ content.js
@@ -1,23 +1,25 @@
 // Log in Javascript console when initialized
 console.log("Welcome to tab50!");
 
-// Run "username" function when window loads
-window.onload = username();
-window.onload = loadtextFunction;
+// Run "loadUsername" function when window loads
+window.onload = loadUsername();
+// Run "loadText" function when window loads
+window.onload = loadText();
 
-// Make sure DOM content has been loaded before running "background" function
-document.addEventListener("DOMContentLoaded", function () {
-    background();
+document.addEventListener("DOMContentLoaded", function() {
+    // Make sure DOM content has been loaded before running "loadBackground" function
+    loadBackground();
     // Once DOM content has loaded, listen for user clicking "Edit Name" button
-    document.querySelector("#username").addEventListener("click", nameFunction);
+    document.querySelector("#username").addEventListener("click", editName);
     // Once DOM content has loaded, listen for user clicking "HUDS Menu" button
-    document.querySelector("#menu").addEventListener("click", menuFunction);
-    // Once DOM content has loaded, listen for user clicking post-it note
-    document.querySelector("#text").addEventListener("click", blankornewFunction);
+    document.querySelector("#menu").addEventListener("click", linkMenu);
+    // Once DOM content has loaded, listen for user clicking on the Post-It Note
+    document.querySelector("#text").addEventListener("click", checkBlank);
 });
 
-// Choose random image from folder
-function background() {
+// Load random background image from folder
+function loadBackground() {
+    // Choose random image from folder
     var r = Math.floor(Math.random() * 50) + 1;
     // Log image number in Javascript console
     console.log("Image number: " + r);
@@ -27,9 +29,8 @@ function background() {
     document.body.style.backgroundImage = "url(" + imgURL + ")";
 }
 
-// Load username from Chrome storage
-// Modified from https://developer.chrome.com/extensions/storage
-function username() {
+// Load username from Chrome storage (modified from https://developer.chrome.com/extensions/storage)
+function loadUsername() {
     username = chrome.storage.sync.get(["username"], function(result) {
         // Log username in Javascript console
         console.log("Username: " + result.username);
@@ -39,22 +40,22 @@ function username() {
     })
 }
 
-// Enable user to change username
-// Modified from https://developer.chrome.com/extensions/storage
-function nameFunction() {
+// Enable user to change username (modified from https://developer.chrome.com/extensions/storage)
+function editName() {
     username = prompt("Please enter your name:");
     // Save username in Chrome storage
     if (username != null) {
-        chrome.storage.sync.set({"username": username}, function() {
+        chrome.storage.sync.set({
+            "username": username
+        }, function() {
             // Log username in Javascript console
             console.log("Username has been changed to: " + username);
         });
     };
-    username();
+    loadUsername();
 }
 
-// Display time and date information on screen
-// Modified from https://www.w3schools.com/js/js_date_methods.asp
+// Display time and date information on screen (modified from https://www.w3schools.com/js/js_date_methods.asp)
 function startTime() {
     // Get the current day based on user location
     var today = new Date();
@@ -68,93 +69,94 @@ function startTime() {
     var day = today.getDay();
     // Creating an array of days (because day is returned as number)
     var days = ["Sunday,", "Monday,", "Tuesday,", "Wednesday,", "Thursday,", "Friday,", "Saturday,", "Sunday,"]
-    // Add a zero in front of single digit minutes with "checkTime" function
+        // Add a zero in front of single digit minutes with "checkTime" function
     m = checkTime(m);
     // Displaying time and greeting the user with relevant greeting based on the time of day
     if (h < 18 && h > 12) {
         var h1 = h - 12;
         document.getElementById("minutes").innerHTML = h1 + ":" + m;
         document.getElementById("greeting").innerHTML = "Good afternoon, " + username + "!";
-    }
-    else if (h < 22 && h >= 18) {
+    } else if (h < 22 && h >= 18) {
         var h2 = h - 12;
         document.getElementById("minutes").innerHTML = h2 + ":" + m;
         document.getElementById("greeting").innerHTML = "Good evening, " + username + "!";
-    }
-    else if (h <= 24 && h >= 22) {
+    } else if (h <= 24 && h >= 22) {
         var h3 = h - 12;
         document.getElementById("minutes").innerHTML = h3 + ":" + m;
         document.getElementById("greeting").innerHTML = "Good night, " + username + "!";
-    }    
-    else if (h < 5 && h > 0) {
+    } else if (h < 5 && h > 0) {
         document.getElementById("minutes").innerHTML = h + ":" + m;
         document.getElementById("greeting").innerHTML = "Good night, " + username + "!";
-    }     
-    else if (h == 0) {
+    } else if (h == 0) {
         var h4 = h + 12;
         document.getElementById("minutes").innerHTML = h4 + ":" + m;
         document.getElementById("greeting").innerHTML = "Good night, " + username + "!";
-    }  
-    else if (h == 12) {
+    } else if (h == 12) {
         document.getElementById("minutes").innerHTML = h + ":" + m;
         document.getElementById("greeting").innerHTML = "Good afternoon, " + username + "!";
-    } 
-    else {
+    } else {
         document.getElementById("minutes").innerHTML = h + ":" + m;
         document.getElementById("greeting").innerHTML = "Good morning, " + username + "!";
     }
     document.getElementById("dayanddate").innerHTML = days[day] + " " + months[month] + " " + date;
-    var t = setTimeout(startTime, 50);
+    // Check time every 500 milliseconds
+    var t = setTimeout(startTime, 500);
 }
 
-// Add a zero in front of single digit minutes (e.g. change "7" to "07")
-// Source: https://www.w3schools.com/js/js_date_methods.asp
+// Add a zero in front of single digit minutes (e.g. change "7" to "07") [source: https://www.w3schools.com/js/js_date_methods.asp]
 function checkTime(m) {
-    if (m < 10) {m = "0" + m}
+    if (m < 10) {
+        m = "0" + m
+    }
     return m;
 }
 
-// Go to HUDS menu of the day
-function menuFunction() {
+// Link to HUDS menu of the day; open in same window once clicked
+function linkMenu() {
     // Getting hour again to avoid using global variable
     var now = new Date();
     var hour = now.getHours();
     // If before 2pm, link to lunch menu, else link to dinner menu
     if (hour <= 13) {
         window.open("http://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?type=30&meal=1", "_self");
-    }
-    else {
+    } else {
         window.open("http://www.foodpro.huds.harvard.edu/foodpro/menu_items.asp?type=30&meal=2", "_self");
     }
 }
 
-function loadtextFunction() {
-    loadtext = chrome.storage.sync.get(["textinput"], function(result) {
-        console.log("Text right now: " + result.textinput);
-        loadtext = result.textinput;
-        document.getElementById("text").innerHTML = loadtext;
+// Load Post-It Note text from Chrome storage, if any (modified from https://developer.chrome.com/extensions/storage)
+function loadText() {
+    loadText = chrome.storage.sync.get(["textinput"], function(result) {
+        console.log("Text loaded: " + result.textinput);
+        loadText = result.textinput;
+        // Display Post-It Note text that had been inputted previously
+        document.getElementById("text").innerHTML = loadText;
     })
 }
 
-function blankornewFunction() {
+// Check if Post-It Note is displaying placeholder text
+function checkBlank() {
+    // If displaying placeholder text, call "blankText" function; else call "newText" function
     if (document.getElementById("text").innerHTML == "Click here to edit me!") {
-        blanktextFunction();
-        console.log("Blank");
-    }
-    else {
-        newtextFunction();
-        console.log("New");
+        blankText();
+    } else {
+        newText();
     }
 }
 
-function blanktextFunction() {
+// Delete placeholder text
+function blankText() {
     document.getElementById("text").innerHTML = "";
 }
 
-function newtextFunction() {
+// Save text inputted by user every 2 seconds when the Post-It Note is edited
+function newText() {
     var textinput = text.innerHTML;
-    chrome.storage.sync.set({"textinput": textinput}, function() {
-        console.log("Text input is: " + textinput);
+    chrome.storage.sync.set({
+        "textinput": textinput
+    }, function() {
+        console.log("New Text: " + textinput);
     })
-    var s = setTimeout(newtextFunction, 2000);
+    // Recursive function to continually save inputted text
+    var s = setTimeout(newText, 2000);
 }
